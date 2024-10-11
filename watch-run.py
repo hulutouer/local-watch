0683599bbc8608cde86c52a496f60b24fe197428
sam <yzysts@126.com>
2024-10-11 08:56:11 +0800

    程序已经基本搞定，后续优化视频成像即可

--- watch-run.py
+++ watch-run.py
@@ -1,26 +1,38 @@
 import os
 import subprocess
 import datetime
+
+
 # 录像文件名
-filename = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-# 存储路径
+def gen_filename():
+    filename = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    return filename
+
+
+# TODO 存储路径
 replay_path = f"/home/sam/replay"
 os.makedirs(replay_path, exist_ok=True)
 
-command = [
-    "ffmpeg",
-    "-f", "v4l2",
-    "-video_size", "1920x1080",
-    "-framerate", "30",  # 30FPS帧数
-    "-i", "/dev/video0",  # 默认摄像头设备
-    "-f", "alsa",
-    "-i", "default",  # 默认音频设备
-    "-c:v", "libx264",
-    "-preset", "ultrafast",
-    "-c:a", "aac",
-    "-t", "180",  # 录制时长180s
-    f"{replay_path}/{filename}.mp4"
-]
+
+def shell_command(replay_name):
+    command = [
+        "ffmpeg",
+        "-f", "v4l2",
+        "-video_size", "1920x1080",
+        "-framerate", "30",  # 30FPS帧数
+        "-i", "/dev/video0",  # 默认摄像头设备
+        "-f", "alsa",
+        "-i", "default",  # 默认音频设备
+        "-c:v", "libx264",
+        # "-preset", "ultrafast",
+        "-preset", "veryfast",
+        "-b:v", "3000k",  # 视频比特率设置为 3000 kbps
+        "-c:a", "aac",
+        "-b:a", "128k",  # 音频比特率设置为 128 kbps
+        "-t", "180",  # 录制时长180s
+        f"{replay_path}/{replay_name}.mp4"
+    ]
+    return command
 
 
 def get_file_creation_time(replay_dir):
@@ -41,23 +53,28 @@ def get_elapsed_time_since_creation(replay_path):
     超过三天进行删除
     """
     for dict_data in get_file_creation_time(replay_path):
-        print(dict_data)
+        # print(dict_data)
         for key in dict_data:
             time_out = dict_data[key]
             # 现在的时间
             current_time = datetime.datetime.now().timestamp()
             elapsed_time = current_time - time_out
-            if elapsed_time > 3600 * 24 * 3:  # 大于三天 删除文件
+            # if elapsed_time > 3600 * 24 * 3:  # 大于三天 删除文件
+            if elapsed_time > 3600:  # 1小时 删除文件
                 os.remove(key)
+
+
+def main():
+    # 删除老文件
+    get_elapsed_time_since_creation(replay_path)
+    # 录像
+    filename = gen_filename()
+    subprocess.run(shell_command(filename), check=True)
 
 
 if __name__ == '__main__':
 
     while True:
-        # 删除老文件
-        get_elapsed_time_since_creation(replay_path)
-        # 录像
-        subprocess.run(command, check=True)
+        main()
 
 # 6行 指定replay路径
-
